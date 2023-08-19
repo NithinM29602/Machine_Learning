d1781596f7be295adb4c056fecd58423f5f2a2a2
Nithin M <92316309+NithinM29602@users.noreply.github.com>
2023-08-19 11:26:29 +0530

    Update color_object_detection.py
    changed the variable. Black --> Color

--- Projects/Color_Object_Detection/color_object_detection.py
+++ Projects/Color_Object_Detection/color_object_detection.py
@@ -2,7 +2,7 @@ from hsv_color_limits import get_color_limits
 import cv2
 from PIL import Image
 
-black = [255,0,0]
+color = [255,0,0]
 # lower_limit = (110,50,50)      # Lower limit for HSV (Hue, Saturation, Value)
 # upper_limit = (130,255,255) # Upper limit for HSV (Hue, Saturation, Value)
 
@@ -13,7 +13,7 @@ while cv2.waitKey(1)!=ord('q'):
     if not has_frame:
         break
     hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_limit, upper_limit = get_color_limits(black)
+    lower_limit, upper_limit = get_color_limits(color)
     mask = cv2.inRange(hsv_image, lower_limit, upper_limit)
 
     mask = Image.fromarray(mask)
